80701408451425ae0ebcb3c337209034580ccbbc
Iakimov Sergey <serg.n.y@yandex.ru>
2021-04-17 18:03:24 +0500

    add right animation with flashlight

--- Dark/Assets/Player/PlayerScript.cs
+++ Dark/Assets/Player/PlayerScript.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Player
@@ -7,22 +9,36 @@ namespace Player
     {
         // Start is called before the first frame update
         private Rigidbody2D body;
-        //private new Transform transform;
-
+        private Animator animator;
+        private string keyUp = "w";
+        private string keyLeft = "a";
+        private string keyDown = "s";
+        private string keyRight = "d";
+        private float speed = 1;
+        
         private void Start()
         {
             body = GetComponent<Rigidbody2D>();
-            //transform = GetComponent<Transform>();
-            Debug.Log("Hello");
+            animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         private void Update()
         {
-            body.velocity = new Vector2(Input.GetAxis("Horizontal")*5, Input.GetAxis("Vertical")*5);
-            if (!Input.anyKey) return;
-            var velocity = body.velocity;
-            //transform.rotation = Quaternion.Euler(0.0f,0.0f,(float)Math.Atan2(velocity.y,velocity.x)*60);
+            var previousVelocity = body.velocity;
+            var horizontalVelocity = (Input.GetKey(keyRight) ? speed : 0) + (Input.GetKey(keyLeft) ? -speed : 0) + 
+                                     previousVelocity.x / 100000; //для плавного перехода между анимациями ходьбы
+            var verticalVelocity = (Input.GetKey(keyUp) ? speed : 0) + (Input.GetKey(keyDown) ? -speed : 0) + 
+                                     previousVelocity.y / 100000; //для плавного перехода между анимациями ходьбы
+            
+            animator.SetBool("isWalk", !(horizontalVelocity == 0 && verticalVelocity == 0));
+            
+            animator.SetBool("isWalkLeft", horizontalVelocity < 0);
+            animator.SetBool("isWalkBack", verticalVelocity < 0 && horizontalVelocity == 0);
+            animator.SetBool("isWalkRight", horizontalVelocity > 0);
+            animator.SetBool("isWalkFront", verticalVelocity > 0 && horizontalVelocity == 0);
+            
+            body.velocity = new Vector2(horizontalVelocity, verticalVelocity);
         }
     }
 }
