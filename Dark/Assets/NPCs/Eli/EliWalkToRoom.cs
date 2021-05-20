d4a1ad94d0fcec74d6e5e2b650988b3687197666
Iakimov Sergey <serg.n.y@yandex.ru>
2021-05-20 19:31:35 +0500

    add walking Eli and tent

--- Dark/Assets/NPCs/Eli/EliWalkToRoom.cs
+++ Dark/Assets/NPCs/Eli/EliWalkToRoom.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,8 @@ using UnityEngine;
 public class EliWalkToRoom : AliveEntity
 {
     private AliveEntity aliveEntity;
-    private float speed = 0.1f;
+    private float speed = 0.2f;
     private Vector2 destination = new Vector2(-5.35f, -2.51f);
-    
     private void Start()
     {
         aliveEntity = this;
@@ -17,16 +17,20 @@ public class EliWalkToRoom : AliveEntity
     {
         aliveEntity.Update();
     }
-    
+
     protected override float GetHorizontalVelocity()
     {
         var bodyPosition = body.transform.position;
-        return bodyPosition.x > destination.x ? speed : -speed;
+        if (Math.Abs(bodyPosition.x - destination.x) < 10e-2)
+            return 0;
+        return bodyPosition.x < destination.x ? speed : -speed;
     }
 
     protected override float GetVerticalVelocity()
     {
         var bodyPosition = body.transform.position;
-        return bodyPosition.y > destination.y ? speed : -speed;
+        if (Math.Abs(bodyPosition.y - destination.y) < 10e-2)
+            return 0;
+        return bodyPosition.y < destination.y ? speed : -speed;
     }
 }
