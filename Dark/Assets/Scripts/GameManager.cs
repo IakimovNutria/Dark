9b7892f7279ba7d8973908d587e1c0611fc15a6e
Iakimov Sergey <serg.n.y@yandex.ru>
2021-05-20 14:59:14 +0500

    fix game start

--- Dark/Assets/Scripts/GameManager.cs
+++ Dark/Assets/Scripts/GameManager.cs
@@ -21,6 +21,7 @@ public class GameManager : MonoBehaviour
 
     public readonly Dictionary<string, bool> StoryBools = new Dictionary<string, bool>
     {
+        {"isGameStarted", false},
         {"isFirstRoomCleaned", false},
         {"isFirstDialogueEnd", false},
         {"isPlayerHelpEli", false}
@@ -45,8 +46,10 @@ public class GameManager : MonoBehaviour
         {
             
         }
-        else if (!GetEnemiesInScene().Any() && !GetDiedInScene().Any())
+        else if (!GetEnemiesInScene().Any() && !GetDiedInScene().Any() && StoryBools["isGameStarted"])
             ChangeStoryBool("isFirstRoomCleaned");
+        else if (GetEnemiesInScene().Any())
+            ChangeStoryBool("isGameStarted");
     }
     
     public void FreezeGame()
