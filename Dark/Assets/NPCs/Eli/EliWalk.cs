8a901c782e61ebb39ce645af28239250a83464a9
Iakimov Sergey <serg.n.y@yandex.ru>
2021-05-21 19:30:48 +0500

    freeze Eli

--- Dark/Assets/NPCs/Eli/EliWalk.cs
+++ Dark/Assets/NPCs/Eli/EliWalk.cs
@@ -9,6 +9,8 @@ public class EliWalk : AliveEntity
     private float speed = 0.2f;
     public Vector2 destination;
     public string storyBoolToChange;
+    public bool isEliGoToBed;
+    
     private new void Start()
     {
         aliveEntity = this;
@@ -20,6 +22,11 @@ public class EliWalk : AliveEntity
         aliveEntity.Update();
         if (Math.Abs(body.position.x - destination.x) < 10e-2 && Math.Abs(body.position.y - destination.y) < 10e-2)
         {
+            body.constraints = RigidbodyConstraints2D.FreezeAll;
+            if (isEliGoToBed)
+                animator.Play("EliBackStandAnimation");
+            else 
+                aliveEntity.SetHealth(0);
             if (!string.IsNullOrEmpty(storyBoolToChange)) 
                 GameManager.GM.ChangeStoryBool(storyBoolToChange, true);
         }
