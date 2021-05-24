623ceeaa5a42cc993fe3065892e600298f3c19b0
Iakimov Sergey <serg.n.y@yandex.ru>
2021-05-24 22:55:06 +0500

    add something to end

--- Dark/Assets/Scripts/Enemy.cs
+++ Dark/Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ public class Enemy : AliveEntity
     private AliveEntity aliveEntity;
     private Transform playerTransform;
     private readonly float damageRadius = 0.5f;
-    private readonly float maxEnemyHealth = 70;
+    private readonly float maxEnemyHealth = 40;
     private Player player;
     private void Awake()
     {
