70329f9f56129cbf619ec700b729b43bf9c7998b
Alexandra Zaytseva <62497070+AlexJackalope@users.noreply.github.com>
2021-05-25 18:12:48 +0500

    Added Pacmario music

--- Dark/Assets/Scripts/MusicManager.cs
+++ Dark/Assets/Scripts/MusicManager.cs
@@ -7,12 +7,23 @@ public class MusicManager : MonoBehaviour
 {
     public AudioSource Stream;
     public AudioSource Waltz;
+    public AudioSource PacMario;
     private void OnLevelWasLoaded(int level)
     {
         if (level == 22)
         {
             Stream.Pause();
             Waltz.Play();
+        }
+        else if (level == 21)
+        {
+            Stream.Pause();
+            PacMario.Play();
+        }
+        else if (level == 16 && GameManager.GM.StoryBools["isPlayerBelieveInMarioPacman"] && GameManager.GM.StoryBools["PlayerAgreeToPlayMarioPacman"])
+        {
+            PacMario.Pause();
+            Stream.Play();
         }
     }
 }
